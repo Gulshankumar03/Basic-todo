86fb10a8b6f27932852d5794449cfa21bf3b5e05
Gulshan Kumar <gulshangk1134@gmail.com>
2024-02-27 19:23:19 +0530

    test 004

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -129,7 +129,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -130,7 +130,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
