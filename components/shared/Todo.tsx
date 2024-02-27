a106ff0de62ec704436a9ce26aec99a0f69d224e
Gulshan Kumar <gulshangk1134@gmail.com>
2024-02-27 18:49:43 +0530

    testing 001

--- components/shared/Todo.tsx
+++ components/shared/Todo.tsx
@@ -9,7 +9,7 @@ const Todo = ({ todo }: { todo: todoType }) => {
   const [editTodo, setEditTodo] = useState(false);
   return (
     <motion.div
-      className=" flex justify-between flex-col 2xl:w-1/4 xl:w-1/3 pt-10 pb-5 px-8 mx-8 lg:mx-0 shadow-xl border-r-1 border-b-1 border-opacity-25 overflow-hidden bg-zinc-400 border-neutral-500 dark:text-white rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10"
+      className=" flex justify-between flex-col 2xl:w-1/4 xl:w-1/3 pt-8 pb-5 px-8 mx-8 lg:mx-0 shadow-xl border-r-1 border-b-1 border-opacity-25 overflow-hidden bg-zinc-400 border-neutral-500 dark:text-white rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
